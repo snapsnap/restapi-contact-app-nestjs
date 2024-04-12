a70fa8b42366c684a99513e0921d9074ba91209b
Ian Aditiawan <ianaditiawan10@gmail.com>
2024-04-12 23:32:58 +0700

    adding create & get addres module

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -6,5 +6,6 @@ import { ContactController } from "./contact.controller";
 @Module({
     providers: [ContactService],
     controllers: [ContactController],
+    exports: [ContactService],
 })
 export class ContactModule {}
---
+++ test/address.spec.ts
@@ -0,0 +1,145 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+import { TestService } from './test.service';
+import { TestModule } from './test.module';
+
+describe('AddressController (e2e)', () => {
+  let app: INestApplication;
+  let logger: Logger;
+  let testService: TestService;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, TestModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    logger = app.get(WINSTON_MODULE_PROVIDER);
+    testService = app.get(TestService);
+  });
+
+  // Test CREATE ADDRESS
+  describe("POST /api/contacts/:contactId/addresses", () => {
+    beforeEach(async () => {
+      await testService.deleteAddress();
+      await testService.deleteContact();
+      await testService.deleteUser();
+
+      await testService.createUser();
+      await testService.createContact();
+
+    });
+
+    it("should be rejected if request is invalid", async () => {
+      const contact = await testService.getContact();
+      const response = await request(app.getHttpServer())
+      .post(`/api/contacts/${contact.id}/addresses`)
+      .set('Authorization', 'test')
+      .send({
+        street: '',
+        city: '',
+        province: '',
+        country: '',
+        postal_code: '',
+      });
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    it("should be able to create address", async () => {
+      const contact = await testService.getContact();
+      const response = await request(app.getHttpServer())
+      .post(`/api/contacts/${contact.id}/addresses`)
+      .set('Authorization', 'test')
+      .send({
+        street: 'jalan test',
+        city: 'kota test',
+        province: 'provinsi test',
+        country: 'negara test',
+        postal_code: '1111',
+      });
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(200);
+      expect(response.body.data.id).toBeDefined();
+      expect(response.body.data.street).toBe('jalan test');
+      expect(response.body.data.city).toBe('kota test');
+      expect(response.body.data.province).toBe('provinsi test');
+      expect(response.body.data.country).toBe('negara test');
+      expect(response.body.data.postal_code).toBe('1111');
+    });
+    
+
+  });
+
+  // Test GET ADDRESS
+  describe("GET /api/contacts/:contactId/addresses", () => {
+    beforeEach(async () => {
+      await testService.deleteAddress();
+      await testService.deleteContact();
+      await testService.deleteUser();
+
+      await testService.createUser();
+      await testService.createContact();
+      await testService.createAddress();
+
+    });
+
+    it("should be rejected if contact is not found", async () => {
+      const contact = await testService.getContact();
+      const address = await testService.getAddress();
+      const response = await request(app.getHttpServer())
+      .get(`/api/contacts/${contact.id + 1}/addresses/${address.id}`)
+      .set('Authorization', 'test')
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(404);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    it("should be rejected if address is not found", async () => {
+      const contact = await testService.getContact();
+      const address = await testService.getAddress();
+      const response = await request(app.getHttpServer())
+      .get(`/api/contacts/${contact.id}/addresses/${address.id + 1}`)
+      .set('Authorization', 'test')
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(404);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    it("should be able to get address", async () => {
+      const contact = await testService.getContact();
+      const address = await testService.getAddress();
+      const response = await request(app.getHttpServer())
+      .get(`/api/contacts/${contact.id}/addresses/${address.id}`)
+      .set('Authorization', 'test')
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(200);
+      expect(response.body.data.id).toBeDefined();
+      expect(response.body.data.street).toBe('jalan test');
+      expect(response.body.data.city).toBe('kota test');
+      expect(response.body.data.province).toBe('provinsi test');
+      expect(response.body.data.country).toBe('negara test');
+      expect(response.body.data.postal_code).toBe('1111');
+    });
+    
+
+  });
+
+});
